1a0e1316e7eb4cf70c65f71fd3d4a7767abfae15
andyhsiungdev <andyhsiungdev@gmail.com>
2015-09-07 10:06:43 -0700

    updated personal info

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
-    'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
+    'name' : 'Andy Hsiung',
+    'bio' : 'Andy Hsiung is a student learning Development on Django.',
+    'email' : 'andy.hsiung.dev@gmail.com', # Leave blank if you'd prefer not to share your email with other conference attendees
+    'twitter_username' : 'andyhsiungdev', # No @ symbol, just the handle.
+    'github_username' : "andyhsiungdev", 
     'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
